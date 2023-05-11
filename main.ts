fae1d762a6b264eeddf82db26ec460569d32db4c
T. Hamilton <profhamilton3@gmail.com>
2023-05-10 20:58:33 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,19 @@ basic.showIcon(IconNames.Heart)
 radio.setTransmitPower(5)
 xgo.init_xgo_serial(SerialPin.P2, SerialPin.P1)
 basic.forever(function () {
+    basic.showIcon(IconNames.Surprised)
     ut = input.magneticForce(Dimension.X)
     if (input.magneticForce(Dimension.X) > 50) {
+        basic.showIcon(IconNames.Angry)
         xgo.execution_action(xgo.action_enum.Request_feeding)
         soundExpression.giggle.playUntilDone()
     } else {
+        basic.showIcon(IconNames.Confused)
         xgo.execution_action(xgo.action_enum.Crawl_forward)
         soundExpression.yawn.playUntilDone()
     }
 })
-loops.everyInterval(100, function () {
+loops.everyInterval(3000, function () {
     radio.sendValue("name", ut)
+    soundExpression.hello.playUntilDone()
 })
