e43851600c3c18118d8a536408d38454e9c2192d
T. Hamilton <profhamilton3@gmail.com>
2023-05-12 19:53:32 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,59 +1,55 @@
-uz = 0
-uy = 0
-ux = 0
-dz = 0
-dy = 0
 dx = 0
+dy = 0
+dz = 0
+ux = 0
+uy = 0
+uz = 0
 buf = bytearray(12)
 radio.set_group(99)
-basic.show_icon(IconNames.HEART)
-radio.set_transmit_power(7)
+basic.show_icon(IconNames.YES)
+radio.set_transmit_power(3)
 xgo.init_xgo_serial(SerialPin.P2, SerialPin.P1)
 
 def on_forever():
     global buf, ux, dx, dy, dz, uy, uz
-    buf = bytearray(8)
-    ux = input.magnetic_force(Dimension.X)
-    dx = input.acceleration(Dimension.X)
-    dy = input.acceleration(Dimension.Y)
-    dz = input.acceleration(Dimension.Z)
-    basic.show_icon(IconNames.SURPRISED)
+    buf = bytearray(12)
+    
     ux = input.magnetic_force(Dimension.X)
     uy = input.magnetic_force(Dimension.Y)
     uz = input.magnetic_force(Dimension.Z)
+    dx = input.acceleration(Dimension.X)
+    dy = input.acceleration(Dimension.Y)
+    dz = input.acceleration(Dimension.Z)
+
+    
     buf.set_number(NumberFormat.INT16_LE, 0, ux)
     buf.set_number(NumberFormat.INT16_LE, 2, uy)
     buf.set_number(NumberFormat.INT16_LE, 4, uz)
     buf.set_number(NumberFormat.INT16_LE, 6, dx)
     buf.set_number(NumberFormat.INT16_LE, 8, dy)
-    buf.set_number(NumberFormat.INT16_LE, 12, dz)
-    if input.magnetic_force(Dimension.Z) > 20:
-        basic.show_icon(IconNames.HAPPY)
-        xgo.execution_action(xgo.action_enum.REQUEST_FEEDING)
-        soundExpression.giggle.play_until_done()
-    else:
-        basic.show_icon(IconNames.CONFUSED)
-        xgo.execution_action(xgo.action_enum.CRAWL_FORWARD)
-        music.play_sound_effect(music.create_sound_effect(WaveShape.SQUARE,
-                4585,
-                4529,
+    buf.set_number(NumberFormat.INT16_LE, 10, dz)
+    if ux > uz:
+        music.play_sound_effect(music.create_sound_effect(WaveShape.NOISE,
+                54,
+                54,
                 255,
                 0,
-                79,
+                500,
                 SoundExpressionEffect.NONE,
-                InterpolationCurve.LOGARITHMIC),
+                InterpolationCurve.LINEAR),
+            SoundExpressionPlayMode.UNTIL_DONE)
+    if uy > uz:
+        music.play_sound_effect(music.create_sound_effect(WaveShape.NOISE,
+                500,
+                499,
+                255,
+                0,
+                750,
+                SoundExpressionEffect.NONE,
+                InterpolationCurve.LINEAR),
             SoundExpressionPlayMode.UNTIL_DONE)
 basic.forever(on_forever)
 
 def on_every_interval():
     radio.send_buffer(buf)
-    music.play_sound_effect(music.create_sound_effect(WaveShape.TRIANGLE,
-            300,
-            200,
-            255,
-            0,
-            75,
-            SoundExpressionEffect.NONE,
-            InterpolationCurve.CURVE),
-        SoundExpressionPlayMode.UNTIL_DONE)
 loops.every_interval(3000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (ux > uz) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+    if (uy > uz) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+})
 let uz = 0
 let uy = 0
 let ux = 0
@@ -6,38 +14,24 @@ let dy = 0
 let dx = 0
 let buf = control.createBuffer(12)
 radio.setGroup(99)
-basic.showIcon(IconNames.Heart)
-radio.setTransmitPower(7)
+basic.showIcon(IconNames.Meh)
+radio.setTransmitPower(3)
 xgo.init_xgo_serial(SerialPin.P2, SerialPin.P1)
-basic.forever(function on_forever() {
-    
-    buf = control.createBuffer(8)
-    ux = input.magneticForce(Dimension.X)
+loops.everyInterval(500, function () {
+    radio.sendBuffer(buf)
+})
+basic.forever(function () {
+    buf = control.createBuffer(12)
+ux = input.magneticForce(Dimension.X)
+    uy = input.magneticForce(Dimension.Y)
+    uz = input.magneticForce(Dimension.Z)
     dx = input.acceleration(Dimension.X)
     dy = input.acceleration(Dimension.Y)
     dz = input.acceleration(Dimension.Z)
-    basic.showIcon(IconNames.Surprised)
-    ux = input.magneticForce(Dimension.X)
-    uy = input.magneticForce(Dimension.Y)
-    uz = input.magneticForce(Dimension.Z)
     buf.setNumber(NumberFormat.Int16LE, 0, ux)
-    buf.setNumber(NumberFormat.Int16LE, 2, uy)
-    buf.setNumber(NumberFormat.Int16LE, 4, uz)
-    buf.setNumber(NumberFormat.Int16LE, 6, dx)
-    buf.setNumber(NumberFormat.Int16LE, 8, dy)
-    buf.setNumber(NumberFormat.Int16LE, 12, dz)
-    if (input.magneticForce(Dimension.Z) > 20) {
-        basic.showIcon(IconNames.Happy)
-        xgo.execution_action(xgo.action_enum.Request_feeding)
-        soundExpression.giggle.playUntilDone()
-    } else {
-        basic.showIcon(IconNames.Confused)
-        xgo.execution_action(xgo.action_enum.Crawl_forward)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 4585, 4529, 255, 0, 79, SoundExpressionEffect.None, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
-    }
-    
-})
-loops.everyInterval(3000, function on_every_interval() {
-    radio.sendBuffer(buf)
-    music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 300, 200, 255, 0, 75, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+buf.setNumber(NumberFormat.Int16LE, 2, uy)
+buf.setNumber(NumberFormat.Int16LE, 4, uz)
+buf.setNumber(NumberFormat.Int16LE, 6, dx)
+buf.setNumber(NumberFormat.Int16LE, 8, dy)
+buf.setNumber(NumberFormat.Int16LE, 10, dz)
 })
